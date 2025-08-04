d3141e590ecfe0ebace7b45db5694fa6cc5cabef
AyushInKC <sirfayush1008@gmail.com>
2025-08-04 15:34:23 +0530

    Project Setup

---
+++ src/main/java/com/FourAM/RepoScribe/RepoScribeApplication.java
@@ -0,0 +1,13 @@
+package com.FourAM.RepoScribe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoScribeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoScribeApplication.class, args);
+	}
+
+}
