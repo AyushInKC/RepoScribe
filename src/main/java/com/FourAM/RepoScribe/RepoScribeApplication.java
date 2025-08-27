bbca9a3472864c34f4d74002eb44540e43a20d54
AyushInKC <sirfayush1008@gmail.com>
2025-08-28 00:27:27 +0530

    WEBCLIENT ERROR

--- src/main/java/com/FourAM/RepoScribe/RepoScribeApplication.java
+++ src/main/java/com/FourAM/RepoScribe/RepoScribeApplication.java
@@ -1,11 +1,8 @@
 package com.FourAM.RepoScribe;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.test.context.TestPropertySource;
 
-import java.io.File;
 
 @SpringBootApplication
 public class RepoScribeApplication {
