dbaab55f8e5518aa9480b31bd1f3696d6ba72516
AyushInKC <sirfayush1008@gmail.com>
2025-09-03 23:58:34 +0530

    SOLVED ERROR

--- src/main/java/com/FourAM/RepoScribe/RepoScribeApplication.java
+++ src/main/java/com/FourAM/RepoScribe/RepoScribeApplication.java
@@ -1,14 +1,16 @@
 package com.FourAM.RepoScribe;
 
+import com.FourAM.RepoScribe.Properties.CohereProperties;
+import com.FourAM.RepoScribe.Properties.GitHubProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 
 @SpringBootApplication
 public class RepoScribeApplication {
-
 	public static void main(String[] args) {
-
 		SpringApplication.run(RepoScribeApplication.class, args);
 	}
 }
+
