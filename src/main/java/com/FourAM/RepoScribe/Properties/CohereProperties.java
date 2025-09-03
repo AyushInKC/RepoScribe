76e394a19b2c481c4fe6066b458cd54a918e3ff9
AyushInKC <sirfayush1008@gmail.com>
2025-09-04 00:17:53 +0530

    ADDED

--- src/main/java/com/FourAM/RepoScribe/Properties/CohereProperties.java
+++ src/main/java/com/FourAM/RepoScribe/Properties/CohereProperties.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
-@Configuration
+
 @ConfigurationProperties(prefix = "cohere")
 public class CohereProperties {
     private String apiKey;
--- src/main/java/com/FourAM/RepoScribe/Properties/GitHubProperties.java
+++ src/main/java/com/FourAM/RepoScribe/Properties/GitHubProperties.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
-@Configuration
+
 @ConfigurationProperties(prefix = "github")
 public class GitHubProperties {
     private String clientId;
