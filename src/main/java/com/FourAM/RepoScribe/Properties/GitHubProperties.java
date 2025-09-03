d8278d4573bb240b22f63d1ee198f8ed3ad5e244
AyushInKC <sirfayush1008@gmail.com>
2025-09-04 00:16:11 +0530

    ADDED

--- src/main/java/com/FourAM/RepoScribe/Properties/GitHubProperties.java
+++ src/main/java/com/FourAM/RepoScribe/Properties/GitHubProperties.java
@@ -1,9 +1,10 @@
 package com.FourAM.RepoScribe.Properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
-@Component
+@Configuration
 @ConfigurationProperties(prefix = "github")
 public class GitHubProperties {
     private String clientId;
--- src/main/java/com/FourAM/RepoScribe/RepoScribeApplication.java
+++ src/main/java/com/FourAM/RepoScribe/RepoScribeApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-
+@EnableConfigurationProperties({GitHubProperties.class,CohereProperties.class})
 @SpringBootApplication
 public class RepoScribeApplication {
 	public static void main(String[] args) {
